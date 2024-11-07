9e0c262f0c59ee84e46c661b181aac468559d78d
Surya D <surya.devireddy.004@gmail.com>
2024-11-07 08:56:35 +0530

    organised

--- server/config.py
+++ server/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@supplier-db.cfaiiyw4ghic.ap-south-1.rds.amazonaws.com/supplier_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "db-uri")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
